a083ced6e39981a49064a8526fa7a50d3cb252de
Samuel Tseng <samuel.tseng@digitty.io>
2019-09-28 15:38:09 +0200

    update twitter access key

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import config
 from flask import Flask, jsonify
 from requests_oauthlib import OAuth1Session
 
-twitter = OAuth1Session('ko18rwNOfM2yADdnKnjtjmi63',
-                        client_secret='nPChoV23IJpQjJDDqVFg64ht5RNMSZ1dfm0ZQdZsfoTfZt8Jyd',
-                        resource_owner_key='977907642-fIu0F5gnwxL9H7gm5ambXHaDbMHhwjLS5HiSEi14',
-                        resource_owner_secret='9OJpJKujDWqcXVdPVy6fesgo6Gj6Hw3ttyJb8h1FtEtOV')
+twitter = OAuth1Session(
+	CLIENT_KEY,
+	client_secret=CLIENT_SECRET,
+	resource_owner_key=ACCESS_KEY,
+    resource_owner_secret=ACCESS_SECRET
+    )
 
-url = 'https://api.twitter.com/1.1/trends/place.json?id=638242'
 app = Flask(__name__)
 
 
@@ -24,8 +25,7 @@ def news():
 def test():
     url = 'https://api.twitter.com/1.1/trends/place.json?id=638242'
     r = twitter.get(url)
-    trends_url = [r.json()['trends'][index]['url'] for index in range(3)]
-    return jsonify(status=True, result=trends_url)
+    return jsonify(status=True, result=r.json())
 
 if __name__ == '__main__':
     app.run()
