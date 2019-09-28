fdcb97fa50e4022933d40a01da0667099ca15c28
Samuel Tseng <samuel.tseng@digitty.io>
2019-09-28 15:45:51 +0200

    fix env variable importing

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, jsonify
 from requests_oauthlib import OAuth1Session
 
 twitter = OAuth1Session(
-	CLIENT_KEY,
-	client_secret=CLIENT_SECRET,
-	resource_owner_key=ACCESS_KEY,
-    resource_owner_secret=ACCESS_SECRET
-    )
+    config.CLIENT_KEY,
+    client_secret=config.CLIENT_SECRET,
+    resource_owner_key=config.ACCESS_KEY,
+    resource_owner_secret=config.ACCESS_SECRET
+)
 
 app = Flask(__name__)
 
@@ -21,11 +21,13 @@ def hello():
 def news():
     return "all is great in alex"
 
+
 @app.route('/test')
 def test():
     url = 'https://api.twitter.com/1.1/trends/place.json?id=638242'
     r = twitter.get(url)
     return jsonify(status=True, result=r.json())
+
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # env = os.environ['WEBSITE_SITE_NAME']
-CLIENT_KEY = os.getenv('CLIENT_KEY')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-ACCESS_KEY = os.getenv('ACCESS_KEY')
-ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+CLIENT_KEY = os.environ('CLIENT_KEY')
+CLIENT_SECRET = os.environ('CLIENT_SECRET')
+ACCESS_KEY = os.environ('ACCESS_KEY')
+ACCESS_SECRET = os.environ('ACCESS_SECRET')
